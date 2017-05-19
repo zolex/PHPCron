1b79112ea2d0cfb79d2d99762fc1e265c899114d
Andreas Linden <andreas.linden@curassist.de>
2017-05-19 09:07:41 +0200

    added removeByComment method and __toString for crontab

--- src/PHPCron/Cron/Tab.php
+++ src/PHPCron/Cron/Tab.php
@@ -8,37 +8,35 @@ use \Symfony\Component\Process\Process;
  */
 class Tab
 {
-    /**
-     * The lines of the cron table, can be cron command or comment
-     *
-     * @var array
-     */
-    protected $lines = array();
+	/**
+	 * The lines of the cron table, can be cron command or comment
+	 *
+	 * @var array
+	 */
+	protected $lines = array();
 
-    /**
-     * The error when using the comment 'crontab'
-     *
-     * @var string
-     */
-    protected $error;
+	/**
+	 * The error when using the comment 'crontab'
+	 *
+	 * @var string
+	 */
+	protected $error;
 
-    /**
-     * The output when using the command 'crontab'
-     *
-     * @var string
-     */
-    protected $output;
+	/**
+	 * The output when using the command 'crontab'
+	 *
+	 * @var string
+	 */
+	protected $output;
 
-    function __construct($loadCurrent = true)
-    {
-		if (true === $loadCurrent) {
-	
-			// parsing cron file
-			$process = new Process('crontab -l');
-			$process->run();
-			$lines = \array_filter(\explode(PHP_EOL, $process->getOutput()), function($line) {
-				return '' != \trim($line);
-			});
+	function __construct()
+	{
+		// parsing cron file
+		$process = new Process('crontab -l');
+		$process->run();
+		$lines = \array_filter(\explode(PHP_EOL, $process->getOutput()), function($line) {
+			return '' != \trim($line);
+		});
 
 			foreach ($lines as $lineNumber => $line) {
 				// if line is not a comment, convert it to a cron
@@ -49,124 +47,149 @@ class Tab
 			}
 
 			$this->error = $process->getErrorOutput();
-		}
-    }
+	}
 
-    /**
-     * Gets the array of crons indexed by line number
-     *
-     * @return array<Job>
-     */
-    public function get()
-    {
-        return \array_filter($this->lines, function($line){
-            return $line instanceof Job;
-        });
-    }
-
-    /**
-     * Add a cron to the cron table
-     *
-     * @param Job $cron
-     * @return PHPCron\Cron\Tab
-     */
-    public function add(Job $cron)
-    {
-        $this->lines[] = $cron;
-
-        return $this;
-    }
-
-    /**
-     * Remove a cron from the cron table
-     *
-     * @param $index The line number
-     * @return PHPCron\Cron\Tab
-     */
-    public function remove($index)
-    {
-        $this->lines = \array_diff_key($this->lines, array($index => ''));
-
-        return $this;
-    }
-
-    /**
-     * Remove all crons which match the given command
-     * 
-     * @param string $command
-     * @return PHPCron\Cron\Tab
-     */
-    public function removeByCommand($command)
-    {
-        foreach ($this->lines as $index => $line) {
-
-            if ($line instanceof Job) {
-
-                if ($command == $line->getCommand()) {
-
-                    $this->remove($index);
-                }
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * Write the current crons in the cron table
-     *
-     * @return PHPCron\Cron\Tab
-     */
-    public function write()
-    {
-        $file = \tempnam(\sys_get_temp_dir(), 'cron');
-
-        \file_put_contents($file, $this->getRaw().PHP_EOL);
-
-        $process = new Process('crontab '.$file);
-        $process->run();
-
-        $this->error = $process->getErrorOutput();
-        $this->output = $process->getOutput();
-
-        return $this;
-    }
-
-    /**
-     * Gets the error output when using the 'crontab' command
-     *
-     * @return string
-     */
-    public function getError()
-    {
-        return $this->error;
-    }
-
-    /**
-     * Gets the output when using the 'crontab' command
-     *
-     * @return string
-     */
-    public function getOutput()
-    {
-        return $this->output;
-    }
-
-    /**
-     * Gets a representation of the cron table file
-     *
-     * @return mixed
-     */
-    public function getRaw()
-    {
-        return \implode(PHP_EOL, $this->lines);
-    }
-	
 	/**
-	 * Clear all jobs from the tab
+	 * Gets the array of crons indexed by line number
+	 *
+	 * @return array<Job>
 	 */
-	public function clear() {
-	
-		$this->lines = array();
+	public function get()
+	{
+		return \array_filter($this->lines, function($line){
+			return $line instanceof Job;
+		});
+	}
+
+	/**
+	 * Add a cron to the cron table
+	 *
+	 * @param Job $cron
+	 * @return PHPCron\Cron\Tab
+	 */
+	public function add(Job $cron)
+	{
+		$this->lines[] = $cron;
+
+		return $this;
+	}
+
+	/**
+	 * Remove a cron from the cron table
+	 *
+	 * @param $index The line number
+	 * @return PHPCron\Cron\Tab
+	 */
+	public function remove($index)
+	{
+		$this->lines = \array_diff_key($this->lines, array($index => ''));
+
+		return $this;
+	}
+
+	/**
+	 * Remove all crons which match the given command
+	 *
+	 * @param string $command
+	 * @return PHPCron\Cron\Tab
+	 */
+	public function removeByCommand($command)
+	{
+		foreach ($this->lines as $index => $line) {
+
+			if ($line instanceof Job) {
+
+				if ($command == $line->getCommand()) {
+
+					$this->remove($index);
+				}
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Remove all crons which match the given comment
+	 *
+	 * @param string $comment
+	 * @return PHPCron\Cron\Tab
+	 */
+	public function removeByComment($comment)
+	{
+		foreach ($this->lines as $index => $line) {
+
+			if ($line instanceof Job) {
+
+				if ($comment == $line->getComment()) {
+
+					$this->remove($index);
+				}
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Write the current crons in the cron table
+	 *
+	 * @return PHPCron\Cron\Tab
+	 */
+	public function write()
+	{
+		$file = \tempnam(\sys_get_temp_dir(), 'cron');
+
+		\file_put_contents($file, $this->getRaw().PHP_EOL);
+
+		$process = new Process('crontab '.$file);
+		$process->run();
+
+		$this->error = $process->getErrorOutput();
+		$this->output = $process->getOutput();
+
+		unlink($file);
+
+		return $this;
+	}
+
+	/**
+	 * Gets the error output when using the 'crontab' command
+	 *
+	 * @return string
+	 */
+	public function getError()
+	{
+		return $this->error;
+	}
+
+	/**
+	 * Gets the output when using the 'crontab' command
+	 *
+	 * @return string
+	 */
+	public function getOutput()
+	{
+		return $this->output;
+	}
+
+	/**
+	 * Gets a representation of the cron table file
+	 *
+	 * @return mixed
+	 */
+	public function getRaw()
+	{
+		return \implode(PHP_EOL, $this->lines);
+	}
+
+	/**
+	 * Magic string method
+	 *
+	 * @return mixed|string
+	 */
+	public function __toString() {
+		 
+		return $this->getRaw();
 	}
 }
